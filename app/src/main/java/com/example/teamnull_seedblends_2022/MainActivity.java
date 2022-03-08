daf663901df43804deab707b704341167c7f78fb
senpeii033 <98488178+senpeii033@users.noreply.github.com>
2022-03-08 13:46:57 -0600

    Login Screen (New Main Activity)

--- app/src/main/java/com/example/teamnull_seedblends_2022/MainActivity.java
+++ app/src/main/java/com/example/teamnull_seedblends_2022/MainActivity.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
+// This is for the certain classes in the XML code
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+
     }
 
 
